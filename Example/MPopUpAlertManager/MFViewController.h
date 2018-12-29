23a02847da19ca4a8604660bbf1d902a532720d2
feng.ma <feng.ma@patpat.com>
2018-12-29 11:34:19 +0800

    Initial commit

---
+++ Example/MPopUpAlertManager/MFViewController.h
@@ -0,0 +1,13 @@
+//
+//  MFViewController.h
+//  MPopUpAlertManager
+//
+//  Created by 1499603656@qq.com on 12/29/2018.
+//  Copyright (c) 2018 1499603656@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MFViewController : UIViewController
+
+@end
---
+++ Example/MPopUpAlertManager/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MPopUpAlertManager
+//
+//  Created by 1499603656@qq.com on 12/29/2018.
+//  Copyright (c) 2018 1499603656@qq.com. All rights reserved.
+//
+
+@import UIKit;
+#import "MFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFAppDelegate class]));
+    }
+}
